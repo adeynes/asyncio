3ea644e1fc50c602b0877225b412341eca1f7529
adeynes <alexisdeynes@outlook.com>
2018-09-12 15:17:27 +0200

    Add write and touch

---
+++ src/adeynes/asyncio/FileTouchAsyncTask.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace adeynes\asyncio;
+
+use pocketmine\Server;
+
+class FileTouchAsyncTask extends AsyncIOTask
+{
+
+    public function onRun(): void
+    {
+        $handle = fopen($this->file, 'w');
+        $this->setSuccess($success = is_resource($handle));
+
+        if ($success) {
+            fclose($handle);
+        }
+    }
+
+    protected function checkSuccess(Server $server): void
+    {
+        if ($this->success) {
+            $server->getLogger()->error("Unable to touch file {$this->file}");
+        } else {
+            $server->getLogger()->debug("Touched file {$this->file}");
+        }
+    }
+
+}
