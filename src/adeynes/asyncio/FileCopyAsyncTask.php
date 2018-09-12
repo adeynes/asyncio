f03dbe257d3ad30af67b2265b5e3d57bde53dd98
adeynes <alexisdeynes@outlook.com>
2018-09-12 15:31:56 +0200

    Delete, move, copy

---
+++ src/adeynes/asyncio/FileCopyAsyncTask.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace adeynes\asyncio;
+
+use pocketmine\Server;
+
+class FileCopyAsyncTask extends AsyncIOTask
+{
+
+    /** @var string */
+    protected $destination;
+
+    public function __construct(string $file, string $destination)
+    {
+        $this->destination = $destination;
+        parent::__construct($file);
+    }
+
+    public function onRun(): void
+    {
+        $this->setSuccess(copy($this->file, $this->destination));
+    }
+
+    protected function checkSuccess(Server $server): void
+    {
+        if ($this->success) {
+            $server->getLogger()->error("Unable to copy file {$this->file} to {$this->destination}");
+        } else {
+            $server->getLogger()->debug("Copied file {$this->file} to {$this->destination}");
+        }
+    }
+
+}
